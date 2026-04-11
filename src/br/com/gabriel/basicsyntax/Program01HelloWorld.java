06cab7d244ef9675eb6d7263c63480e5e3d50631
Gabriel Melo Ribeiro <meloribeiro0757@gmail.com>
2026-04-11 19:38:22 -0300

    refactor: rename HelloWorld.java to Program01HelloWorld.java

--- src/br/com/gabriel/basicsyntax/Program01HelloWorld.java
+++ src/br/com/gabriel/basicsyntax/Program01HelloWorld.java
@@ -1,6 +1,6 @@
 package br.com.gabriel.basicsyntax;
 
-public class HelloWorld {
+public class Program01HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
