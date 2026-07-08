b9e5e113e4c507470f9c755dc274b54fea5cdcc8
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-07-08 17:29:35 -0300

    feat: add program with basic for usage

---
+++ src/br/com/gabriel/basicsyntax/Program06For.java
@@ -0,0 +1,9 @@
+package br.com.gabriel.basicsyntax;
+
+public class Program06For {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++) {
+            System.out.println("Iteration number " + i + " inside the For statement");
+        }
+    }
+}
