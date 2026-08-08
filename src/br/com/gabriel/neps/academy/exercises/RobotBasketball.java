4b13215daf7fc1c7685727b46ce116f2f3d6bd50
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-08-08 16:01:52 -0300

    feat: add program to solve the 'Robot Basketball' exercise from NepsAcademy

---
+++ src/br/com/gabriel/neps/academy/exercises/RobotBasketball.java
@@ -0,0 +1,19 @@
+package br.com.gabriel.neps.academy.exercises;
+
+import java.util.Scanner;
+
+public class RobotBasketball {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int D = scanner.nextInt();
+        
+        if (D <= 800) {
+            System.out.println(1);
+        } else if (D <= 1400) {
+            System.out.println(2);
+        } else {
+            System.out.println(3);
+        }
+    }
+}
