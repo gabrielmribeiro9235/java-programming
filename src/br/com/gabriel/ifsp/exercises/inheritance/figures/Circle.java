a550db877762d7ad672c12524417bec243a0d2d4
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-13 12:27:16 -0300

    feat: create Circle class

---
+++ src/br/com/gabriel/ifsp/exercises/inheritance/figures/Circle.java
@@ -0,0 +1,19 @@
+package br.com.gabriel.ifsp.exercises.inheritance.figures;
+
+public final class Circle extends Figure {
+    private final double radius;
+
+    public Circle(double x, double y, double radius) {
+        super(x, y);
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
