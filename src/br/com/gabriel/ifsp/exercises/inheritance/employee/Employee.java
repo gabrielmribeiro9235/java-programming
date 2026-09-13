644500751c7a947ac2416936e42a311758389209
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-13 18:40:50 -0300

    refactor: clean equals method of the Employee class

--- src/br/com/gabriel/ifsp/exercises/inheritance/employee/Employee.java
+++ src/br/com/gabriel/ifsp/exercises/inheritance/employee/Employee.java
@@ -36,8 +36,7 @@ public abstract sealed class Employee permits FullTimeEmployee, PerHourEmployee 
 
     @Override
     public boolean equals(Object o) {
-        if (!(o instanceof Employee)) return false;
-        Employee employee = (Employee) o;
+        if (!(o instanceof Employee employee)) return false;
         return Objects.equals(id, employee.id);
     }
 
