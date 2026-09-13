b6b0d54ae7bb514858f4905e2b98a9ede448e238
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-13 17:11:09 -0300

    feat: create Employee class

---
+++ src/br/com/gabriel/ifsp/exercises/inheritance/employee/Employee.java
@@ -0,0 +1,48 @@
+package br.com.gabriel.ifsp.exercises.inheritance.employee;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public abstract sealed class Employee permits FullTimeEmployee, PerHourEmployee {
+    private final String id;
+    private final String name;
+    private final String jobTitle;
+    private final LocalDate dateOfEmployment;
+
+    public Employee(String id, String name, String jobTitle, LocalDate dateOfEmployment) {
+        this.id = id;
+        this.name = name;
+        this.jobTitle = jobTitle;
+        this.dateOfEmployment = dateOfEmployment;
+    }
+
+    public abstract double salary();
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public LocalDate getDateOfEmployment() {
+        return dateOfEmployment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
