d981b4c1f001cf3a29b466fda6f6c61aeac6c817
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-13 17:25:45 -0300

    feat: create FullTimeEmployee class

---
+++ src/br/com/gabriel/ifsp/exercises/inheritance/employee/FullTimeEmployee.java
@@ -0,0 +1,33 @@
+package br.com.gabriel.ifsp.exercises.inheritance.employee;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public final class FullTimeEmployee extends Employee {
+    private double monthlySalary;
+
+    public FullTimeEmployee(String id, String name, String jobTitle, LocalDate dateOfEmployment, double monthlySalary) {
+        super(id, name, jobTitle, dateOfEmployment);
+        this.monthlySalary = monthlySalary;
+    }
+
+    public void setMonthlySalary(double monthlySalary) {
+        this.monthlySalary = monthlySalary;
+    }
+
+    @Override
+    public double salary() {
+        return monthlySalary;
+    }
+
+    @Override
+    public String toString() {
+        return "FullTimeEmployee{" +
+                "id='" + getId() + '\'' +
+                ", name='" + getName() + '\'' +
+                ", jobTitle='" + getJobTitle() + '\'' +
+                ", dateOfEmployment='" + getDateOfEmployment().format(DateTimeFormatter.ofPattern("mm/dd/yyyy")) + '\'' +
+                ", monthlySalary=" + monthlySalary +
+                '}';
+    }
+}
