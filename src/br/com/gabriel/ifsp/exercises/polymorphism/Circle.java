b39c1edaa5790ac89aa8548800c5a64d1d7a18e5
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-17 11:39:53 -0300

    feat: create the Circle class

---
+++ src/br/com/gabriel/ifsp/exercises/polymorphism/Circle.java
@@ -0,0 +1,47 @@
+package br.com.gabriel.ifsp.exercises.polymorphism;
+
+import java.util.Objects;
+
+public class Circle implements Figure {
+    private final double radius;
+
+    public Circle(double radius) {
+        if (isRadiusValid(radius)) {
+            this.radius = radius;
+        } else {
+            throw new IllegalArgumentException("Invalid radius value");
+        }
+    }
+
+    private static boolean isRadiusValid(double radius) {
+        return radius > 0;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + getRadius() +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Circle circle = (Circle) o;
+        return Double.compare(getRadius(), circle.getRadius()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getRadius());
+    }
+
+    @Override
+    public double area() {
+        return Math.pow(radius, 2) * Math.PI;
+    }
+}
