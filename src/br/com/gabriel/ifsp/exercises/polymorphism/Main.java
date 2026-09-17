4fec6fd6e13c3fdbb8b75c089a45529bc6afdd08
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-17 12:34:16 -0300

    feat: add Main class to test Figure interface and Circle, Rectangle, and Triangle classes

---
+++ src/br/com/gabriel/ifsp/exercises/polymorphism/Main.java
@@ -0,0 +1,27 @@
+package br.com.gabriel.ifsp.exercises.polymorphism;
+
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        Figure[] figures = new Figure[150];
+
+        for (int i = 0; i < figures.length; i++) {
+            if (i < 50) {
+                figures[i] = Circle.create(i + 1);
+            } else if (i < 100) {
+                figures[i] = Rectangle.create(i - 49);
+            } else {
+                figures[i] = Triangle.create(i - 99);
+            }
+        }
+
+        double sumOfTheAreas = 0;
+
+        for (Figure figure : figures) {
+            sumOfTheAreas += figure.area();
+        }
+
+        System.out.printf(Locale.US, "The sum of the areas of the 150 figures is: %.3f\n", sumOfTheAreas);
+    }
+}
