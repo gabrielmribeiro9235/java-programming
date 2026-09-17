455c1d3686da685fce1bb1f31b9989ebd13a4c71
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-17 11:48:17 -0300

    feat: create the Rectangle class

---
+++ src/br/com/gabriel/ifsp/exercises/polymorphism/Rectangle.java
@@ -0,0 +1,54 @@
+package br.com.gabriel.ifsp.exercises.polymorphism;
+
+import java.util.Objects;
+
+public class Rectangle implements Figure {
+    private final double width;
+    private final double length;
+
+    public Rectangle(double width, double length) {
+        if (isArgumentValid(width) && isArgumentValid(length)) {
+            this.width = width;
+            this.length = length;
+        } else {
+            throw new IllegalArgumentException("Invalid width and/or length value");
+        }
+    }
+
+    private static boolean isArgumentValid(double arg) {
+        return arg > 0;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + getWidth() +
+                ", length=" + getLength() +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return Double.compare(getWidth(), rectangle.getWidth()) == 0 && Double.compare(getLength(), rectangle.getLength()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getWidth(), getLength());
+    }
+
+    @Override
+    public double area() {
+        return width * length;
+    }
+}
