717922f5626d8ed65cc55cd8cab0328e26dc5cfe
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-18 10:08:59 -0300

    feat: add Main class to test Animal, Owl, Lion, Wolf, and Zoo classes

---
+++ src/br/com/gabriel/ifsp/exercises/polymorphism/zoo/Main.java
@@ -0,0 +1,9 @@
+package br.com.gabriel.ifsp.exercises.polymorphism.zoo;
+
+public class Main {
+    public static void main(String[] args) {
+        Zoo zoo = Zoo.createZoo();
+
+        zoo.makeTheAnimalsMakeSound();
+    }
+}
