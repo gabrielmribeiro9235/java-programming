1be0945a12bcebd71f13e8703034f090e1b9ba97
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-18 17:23:53 -0300

    feat: create the FakeEmployeeRepository class

---
+++ src/br/com/gabriel/ifsp/exercises/polymorphism/employee/FakeEmployeeRepository.java
@@ -0,0 +1,52 @@
+package br.com.gabriel.ifsp.exercises.polymorphism.employee;
+
+import java.util.StringJoiner;
+
+public class FakeEmployeeRepository implements Repository<Employee, String> {
+    private Employee[] employees;
+    private int numberOfEmployees;
+
+    public FakeEmployeeRepository() {
+        employees = new Employee[10];
+    }
+
+    private void increaseArraySize() {
+        Employee[] copy = employees.clone();
+
+        employees = new Employee[numberOfEmployees * 2];
+
+        for (int i = 0; i < numberOfEmployees; i++) {
+            employees[i] = copy[i];
+        }
+    }
+
+    @Override
+    public void saveEntity(Employee entity) {
+        if (numberOfEmployees == employees.length) {
+            increaseArraySize();
+        }
+
+        employees[numberOfEmployees++] = entity;
+    }
+
+    @Override
+    public Employee findById(String id) {
+        for (int i = 0; i < numberOfEmployees; i++) {
+            if (employees[i].getId().equals(id)) {
+                return employees[i];
+            }
+        }
+
+        return null;
+    }
+
+    public String showSavedEmployees() {
+        StringJoiner joiner = new StringJoiner("\n");
+
+        for (int i = 0; i < numberOfEmployees; i++) {
+            joiner.add(employees[i].toString());
+        }
+
+        return joiner.toString();
+    }
+}
