1bd2d0ce807cfea69dc0737b11f87cf500d1625f
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-25 20:50:52 -0300

    refactor: improve the solution of the 'Magic Square' exercise

--- src/br/com/gabriel/neps/academy/exercises/MagicSquare.java
+++ src/br/com/gabriel/neps/academy/exercises/MagicSquare.java
@@ -9,48 +9,40 @@ public class MagicSquare {
         int n = scanner.nextInt();
 
         int[][] mat = new int[n][n];
-
-        int rowSum = 0, magicSum = 0;
-        boolean isMagic = true;
+        int[] rowSums = new int[n];
+        int[] columnSums = new int[n];
+        int diagonal1 = 0, diagonal2 = 0;
+        int magicSum = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 mat[i][j] = scanner.nextInt();
-                rowSum += mat[i][j];
-            }
-            if (i != 0 && magicSum != rowSum) {
-                isMagic = false;
-            } else if (i == 0) {
-                magicSum = rowSum;
-            }
-            rowSum = 0;
-        }
 
-        if (!isMagic) {
-            System.out.println(-1);
-            return;
-        }
+                rowSums[i] += mat[i][j];
+                columnSums[j] += mat[i][j];
 
-        for (int i = 0; i < n; i++) {
-            int columnSum = 0;
-            for (int j = 0; j < n; j++) {
-                columnSum += mat[j][i];
-            }
-            if (columnSum != magicSum) {
-                System.out.println(-1);
-                return;
-            }
-        }
+                if (i == j) {
+                    diagonal1 += mat[i][j];
+                }
 
-        int diagonal1 = 0, diagonal2 = 0;
-        for (int i = 0; i < n; i++) {
-            diagonal1 += mat[i][i];
-            diagonal2 += mat[i][n - i - 1];
+                if (i + j + 1 == n) {
+                    diagonal2 += mat[i][j];
+                }
+            }
+
+            if (i == 0) magicSum = rowSums[i];
         }
 
         if (diagonal1 != magicSum || diagonal2 != magicSum) {
             System.out.println(-1);
             return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (columnSums[i] != magicSum || rowSums[i] != magicSum) {
+                System.out.println(-1);
+                return;
+            }
         }
 
         System.out.println(magicSum);
