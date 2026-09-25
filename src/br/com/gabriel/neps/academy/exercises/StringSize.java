c8c96bd823de03c9b6a5a8a99e877521d253fc8e
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-25 20:57:38 -0300

    feat: add program to solve the 'String Size' exercise from NepsAcademy

---
+++ src/br/com/gabriel/neps/academy/exercises/StringSize.java
@@ -0,0 +1,11 @@
+package br.com.gabriel.neps.academy.exercises;
+
+import java.util.Scanner;
+
+public class StringSize {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(scanner.nextLine().length());
+    }
+}
